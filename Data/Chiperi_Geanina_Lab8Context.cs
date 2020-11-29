00a1f4934fc94023f7af363832f40f3492fd2669
Geanina <chiperi.geanina@gmail.com>
2020-11-29 15:47:46 +0200

    Add project files.

---
+++ Data/Chiperi_Geanina_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Chiperi_Geanina_Lab8.Models;
+
+namespace Chiperi_Geanina_Lab8.Data
+{
+    public class Chiperi_Geanina_Lab8Context : DbContext
+    {
+        public Chiperi_Geanina_Lab8Context (DbContextOptions<Chiperi_Geanina_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Chiperi_Geanina_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Chiperi_Geanina_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Chiperi_Geanina_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Chiperi_Geanina_Lab8.Data;
+using Chiperi_Geanina_Lab8.Models;
+
+namespace Chiperi_Geanina_Lab8.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Chiperi_Geanina_Lab8.Data.Chiperi_Geanina_Lab8Context _context;
+
+        public DeleteModel(Chiperi_Geanina_Lab8.Data.Chiperi_Geanina_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FindAsync(id);
+
+            if (Category != null)
+            {
+                _context.Category.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
